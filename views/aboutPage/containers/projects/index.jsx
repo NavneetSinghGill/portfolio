fe41a2d1c1e818a396698ec9524198a33699fc18
Navneet Singh Gill <navneet105gill@gmail.com>
2021-02-02 23:36:09 -0500

    Update projects

--- views/aboutPage/containers/projects/index.jsx
+++ views/aboutPage/containers/projects/index.jsx
@@ -51,14 +51,15 @@ const Projects = () => {
             <Entity 
                 name="CasaDigi"
                 workingDuration="(Aug 2018 - July 2019)"
-                skills="iOS, Swift, IoT, Socket, Git"
+                skills="iOS, Swift, IoT, Socket, Git, Swift, Autolayout, RSA, VIPER, MVC, MVVM"
                 link='https://apps.apple.com/us/app/casadigi-3-0/id1487106225'
                 appstore='https://apps.apple.com/us/app/casadigi-3-0/id1487106225'
                 points = {[
-                    "Collaborated with the team on how to design and implement CasaDigi iOS Application",
-                    "Worked with the backend team to discuss the best way possible for the development of new features",
-                    "For the most flexibility, myself with my team hand-coded all the required components",
-                    "Encouraged use of project management within the team to keep track of the product effectively",
+                    "Designed UX and implemented the architecture of IoT application using Swift and Objective-C with bridging header",
+                    "Spatial reasoning in designing and building the IoT application architecture with 1 touch access in mind",
+                    "Created intuitive design, responsive to all iOS devices and did Data preparation, storage and encryption",
+                    "Integrated RESTful APIs and Sockets for CRUD operations and hardware (TV, music system, AC, etc) control using iPad and iPhone",
+                    "Scrum and Agile proficiency with JIRA along with code management using Git on GitHub and Bitbucket",
                 ]}
             />
 
@@ -82,10 +83,20 @@ const Projects = () => {
                 github="https://github.com/NavneetSinghGill/MentalSnapp"
                 appstore="https://apps.apple.com/gb/app/mental-snapp/id1187218345"
                 points = {[
-                    "Collaborated with the team on how to design and implement CasaDigi iOS Application",
-                    "Worked with the backend team to discuss the best way possible for the development of new features",
+                    "Implemented audio/video to record the emotion of patients",
+                    "Added Chat functionality to let doctors assist patients and used AWS-S3 for storage and analysis",
                     "For the most flexibility, myself with my team hand-coded all the required components",
                     "Encouraged use of project management within the team to keep track of the product effectively",
+                ]}
+            />
+
+            <Entity         
+                name="SpiralPay"
+                workingDuration="2017"
+                skills="Swift, VIPER, Facial recognition, Ecommerce, Inventory, QR Scan"
+                points = {[
+                    "Designed and developed this payment gateway application where users can do payment on the go just by scanning the products QR.",
+                    "Implemented functional ecommerce platform inclusive of Microsoft backend database",
                 ]}
             />
             
@@ -96,10 +107,22 @@ const Projects = () => {
                 link='https://github.com/NavneetSinghGill/Peruze'
                 github="https://github.com/NavneetSinghGill/Peruze"
                 points = {[
-                    "Collaborated with the team on how to design and implement CasaDigi iOS Application",
-                    "Worked with the backend team to discuss the best way possible for the development of new features",
-                    "For the most flexibility, myself with my team hand-coded all the required components",
-                    "Encouraged use of project management within the team to keep track of the product effectively",
+                    "Created iCloud based backed to store user data making it independent of any other technology than iOS",
+                    "Implemented smooth animations using UIImageView and components for reusability",
+                    "Used developer portal to design the database for CRUD operations",
+                ]}
+            />
+
+            <Entity 
+                name="Hamperville"
+                workingDuration="Winter 2016"
+                skills="Objective-C, RestFul APIs, Classical design"
+                link='https://github.com/NavneetSinghGill/Hamperville'
+                github="https://github.com/NavneetSinghGill/Hamperville"
+                points = {[
+                    "Implement this Laundry app which included pickup, drop-off options with traditional app design",
+                    "Created 10+ pixel-perfect screens with pleasing designs for user engagement",
+                    "Gather and filter requirements to include only the most suitable information in the best way possible",
                 ]}
             />
 
