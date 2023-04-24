48dea6b75b102d9d9081f2a1f0a40b7d8a058850
Navneet Singh Gill <gillsupremacy@navneets-mbp.home>
2023-04-24 18:09:33 -0400

    update the profile information

--- views/aboutPage/containers/aboutInfo.jsx
+++ views/aboutPage/containers/aboutInfo.jsx
@@ -7,12 +7,12 @@ const AboutInfo = () => {
                     ABOUT
                 </h2>
                 <p className="generalIntro-subheading n-left">
-                    I am a passionate self-driven <b>Software Engineer</b> with over 6 years of mobile development experience
+                    I am a passionate self-driven Software Engineer with over <b>6 years</b> of mobile development experience
                     I currently live in Mississauga ON Canada.
                 </p>
                 <p className="generalIntro-subheading n-left">
                     I grew up in India and my love for tech led me to pursue a Bachelor of Engineering in IT. 
-                    I further worked mostly as an iOS and MERN stack application developer for about <b>4+ years</b> which drastically improved my problem solving and analytical skills.
+                    I further worked mostly as an iOS and MERN stack application developer for about 4+ years which drastically improved my problem solving and analytical skills.
                 </p>
                 <p className="generalIntro-subheading n-left">
                     Later, I pursued a Master of Applied Computing from the University of Windsor and then worked with CIBC and HSBC as a Senior iOS Developer. 
